c3dcb02fe3b10caf32dae404aa25fec09a6d201f
Dominique <dominique@0587370210.wireless.umich.net>
2019-04-10 14:49:06 -0400

    lyft to date

--- lyft_info.py
+++ lyft_info.py
@@ -3,7 +3,7 @@ from lyft_rides.session import Session
 
 import lyft_tokens
 import json
-
+from lyft_rides.client import LyftRidesClient
 
 auth_flow = ClientCredentialGrant(
     lyft_tokens.YOUR_CLIENT_ID,
@@ -11,9 +11,23 @@ auth_flow = ClientCredentialGrant(
     lyft_tokens.YOUR_PERMISSION_SCOPES,
     )
 session = auth_flow.get_session()
-
-from lyft_rides.client import LyftRidesClient
 client = LyftRidesClient(session)
-response = client.get_cost_estimates(37.7763, -122.391, 37.7972, -122.4533)
-ride_types = response.json.get('cost_estimates')
-print(ride_types)
+
+response = client.get_cost_estimates(
+    start_latitude = 37.7766,
+    start_longitude = -122.391,
+    end_latitude = 37.7972,
+    end_longitude = -122.4533,
+    ride_type = 'lyft'
+)
+estimate = response.json.get('cost_estimates')
+distance = estimate[0]['estimated_distance_miles']
+costmin = estimate[0]['estimated_cost_cents_min']
+costmax = estimate[0]['estimated_cost_cents_max']
+
+print(distance)
+print(costmin)
+print(costmax)
+
+# for line in estimate:
+#     print(line)
